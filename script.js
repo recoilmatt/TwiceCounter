695bdab47cb0217301ded4e982ff8b7337a3bf97
Matt <recoilmatt@gmail.com>
2019-07-02 22:44:32 -0700

    update2
    updated

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 let appId = 'a2EKEhAR1KwNd5wQPVb3tIZ8dkSlMWZ3'
 
-document.addEventListener('DOMContentLoaded', function () {
-	q = "twice kpop"; // search query
-
+document.addEventListener('DOMContentLoaded', function twiceLoad() {
+	q = "twice+tt+kpop"; // search query
+	function request() {
 	request = new XMLHttpRequest;
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=a2EKEhAR1KwNd5wQPVb3tIZ8dkSlMWZ3&limit=30&tag='+q, true);
-
+}
+	request();
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data = JSON.parse(request.responseText).data.image_url;
@@ -42,7 +43,32 @@ dahyun.onclick = function() {
 
 }
 
+tzuyu.onclick = function twiceLoad() {
+	q = "tzuyu+twice+저우쯔위"; // search query
+	function request() {
+	request = new XMLHttpRequest;
+	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=a2EKEhAR1KwNd5wQPVb3tIZ8dkSlMWZ3&limit=30&tag='+q, true);
+}
+	request();
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400){
+			data = JSON.parse(request.responseText).data.image_url;
+			console.log(data);
+			document.getElementById("twiceGif").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+		} else {
+			console.log('reached giphy, but API returned an error');
+		 }
+	};
 
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+	request.send();
+}
+
+	//let image = 'https://media0.giphy.com/media/l1JN7FkjsUICp5fWg/giphy.gif?cid=790b76115d1aeb724a54736a77fd1fda&rid=giphy.gif'
+	//document.getElementById("twiceGif").innerHTML = "<img src=" + image + " />";
 
 // Set the date we're counting down to
 var countDownDate = new Date("Jul 17, 2019 19:30:25").getTime();
